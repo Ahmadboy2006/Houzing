9c25b7b8a7d20d93244d4c06f60d45121b2da772
AXA_developer <ahmadhasanboyev@gmail.com>
2025-03-26 18:31:36 +0500

    sign out succesfull

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Profile() {
+  const navigate = useNavigate();
+
+  const signOut = () => {
+    localStorage.removeItem("token");
+    window.dispatchEvent(new Event("storage"));
+    navigate("/");
+  }
+
   return (
     <div>
-      Profile
+      <h2>Profile</h2>
+      <button onClick={signOut}>Sign Out</button>
     </div>
   )
 }
