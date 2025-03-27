93b6d1ba9cae45651088a18a1f13e5b5d677478c
Ravshanbek0 <rnabijonov66@gmail.com>
2025-03-27 23:24:09 +0500

    Profile page added

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Profile.css'
+import axios from 'axios';
+import { FaUser } from "react-icons/fa6";
 
 function Profile() {
   const navigate = useNavigate();
+  const [userData, setUserData] = useState()
+  const [name, setName] = useState("Ism")
+  const [telephone, setTelephone] = useState("Tel raqam")
+  const [email, setEmail] = useState("Email")
+  const [showSignOut, setShowSignOut] = useState(false);
+
+  const toggleSignOut = () => {
+    setShowSignOut(prev => !prev);
+  };
 
   const signOut = () => {
     localStorage.removeItem("token");
@@ -10,10 +22,69 @@ function Profile() {
     navigate("/");
   }
 
+  function userPorfile(params) {
+    const token = localStorage.getItem('token');
+    console.log(token);
+
+    if (token) {
+      const response = axios.get('https://houzing-api.up.railway.app/api/users/profile', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(response);
+
+    }
+  }
+  useEffect(() => {
+    // userPorfile()
+  }, [])
+
   return (
-    <div>
-      <h2>Profile</h2>
-      <button onClick={signOut}>Sign Out</button>
+    <div className="profile-container">
+      <h2 className="title"><span className='user-icon'><FaUser /></span> Profile</h2>
+
+      <div className="profile-content">
+        <div className="form-section">
+          <div className="input-group">
+            <label>First name</label>
+            <input type="text" value={name} placeholder="Enter first name" disabled/>
+          </div>
+
+          <div className="input-group">
+            <label>Phone number</label>
+            <input type="text" value={telephone} placeholder="Enter phone number" disabled/>
+          </div>
+
+          {/* <div className="input-group">
+            <label>Language</label>
+            <select>
+              <option>English</option>
+              <option>Uzbek</option>
+              <option>Russian</option>
+            </select>
+          </div> */}
+        </div>
+
+        <div className="form-section">
+          <div className="input-group">
+            <label>Email address</label>
+            <input type="email" value={email} placeholder="Email" disabled />
+          </div>
+
+          <div className="input-group">
+            <label>Password</label>
+            <button className="change-pass-btn">Change password</button>
+          </div>
+        </div>
+
+        
+      </div>
+
+      <div className="save-btn-wrapper">
+        <button onClick={signOut} className="signout-btn">Sign out</button>
+        <Link to={'/myprops'}><button className="save-btn">My products</button></Link>
+      </div>
     </div>
   )
 }
